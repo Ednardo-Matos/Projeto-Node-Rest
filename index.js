1031a3035d7893cfc396e0bfb117db7b1cb2b722
Ednardo-Matos <ednardoinacio@outlook.com>
2021-10-01 21:28:24 -0300

    adicionando infraestrutura e model

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const customExpress = require('./config/customExpress')
 const port = 3000 // atribuir uma porta ao servidor
 const conexao = require('./infraestrutura/conexao.js')
+const Tabelas = require('./infraestrutura/tabelas')
+
 
 conexao.connect(erro => {
     if(erro){
         console.log(erro)
     }else{
         console.log('Conectado com sucesso')
+
+        Tabelas.init(conexao)
+
+        const app = customExpress()
+
+        app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
     }
 })
-const app = customExpress()
 
-
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
